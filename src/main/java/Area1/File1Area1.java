379b493f012d83e0aa8d5ca2af35c086ff61cbd0
invertednz <jamesfarrier@gmail.com>
2018-11-30 12:05:09 -1000

    TestRun6Commit

--- src/main/java/Area1/File1Area1.java
+++ src/main/java/Area1/File1Area1.java
@@ -4,5 +4,5 @@ package Area1;
  * Created by james on 30/11/2018.
  */
 public class File1Area1 {
-    int i = 0;
+    int i = 1;
 }
---
+++ src/main/java/Area2/File2Area2.java
@@ -0,0 +1,7 @@
+package Area2;
+
+/**
+ * Created by james on 30/11/2018.
+ */
+public class File2Area2 {
+}
