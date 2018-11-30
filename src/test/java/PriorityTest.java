bdfeb07056b727bf599d9f5b9b1e8cc462cbfebc
invertednz <jamesfarrier@gmail.com>
2018-11-30 11:49:56 -1000

    Testing Priority

---
+++ src/test/java/PriorityTest.java
@@ -0,0 +1,58 @@
+/**
+ * Created by james on 30/11/2018.
+ */
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PriorityTest {
+    @Test
+    public void Test1() {
+        Assert.assertTrue(true);
+        System.out.println("Pass Test case - testOne");
+    }
+
+    @Test
+    public void Test2() {
+        Assert.assertTrue(true);
+        System.out.println("Pass Test case - testOne");
+    }
+
+    @Test
+    public void Test3() {
+        Assert.assertTrue(true);
+        System.out.println("Pass Test case - testOne");
+    }
+
+    @Test
+    public void Test4() {
+        Assert.assertTrue(true);
+        System.out.println("Pass Test case - testOne");
+    }
+
+    @Test
+    public void Test5() {
+        Assert.assertTrue(true);
+        System.out.println("Pass Test case - testOne");
+    }
+
+    @Test
+    public void Test6() {
+        Assert.assertTrue(true);
+        System.out.println("Pass Test case - testOne");
+    }
+
+    @Test
+    public void Test7() {
+        Assert.assertTrue(true);
+        System.out.println("Pass Test case - testOne");
+    }
+
+    @Test
+    public void Test8() {
+        Assert.assertTrue(true);
+        System.out.println("Pass Test case - testOne");
+    }
+
+}
